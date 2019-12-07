4fd340cfe50bd3a06327fe40517d83d2df1ec7a3
Pasan Godamune <pasanjg@gmail.com>
2019-12-08 01:47:59 +0530

    system update on users and db

--- src/com/gamestation/service/UserServiceImpl.java
+++ src/com/gamestation/service/UserServiceImpl.java
@@ -12,8 +12,7 @@ public class UserServiceImpl implements IUserService {
 
 	public void addUser(User user) {
 
-		String addUserQuery = "INSERT INTO user VALUES(?,?,?,?,?,?,?,?,?)";
-		String addUserAccountQuery = "INSERT INTO useraccount VALUES(?,?,?)";
+		String addUserQuery = "INSERT INTO user VALUES(?,?,?,?,?,?,?,?,?,?)";
 
 		try {
 			// add data to user table
@@ -26,22 +25,13 @@ public class UserServiceImpl implements IUserService {
 			ps.setString(5, user.getCountry());
 			ps.setString(6, user.getPlatform());
 			ps.setString(7, user.getUserName());
-			ps.setString(8, user.getEmail());
-			ps.setString(9, user.getType());
-
-			ps.executeUpdate();
-
-			// add data to useraccount table
-			ps = DBConnection.getDBconnection().prepareStatement(addUserAccountQuery);
-
-			ps.setString(1, user.getUserID());
-			ps.setString(2, user.getUserName());
-			ps.setString(3, user.getPassword());
+			ps.setString(8, user.getPassword());
+			ps.setString(9, user.getEmail());
+			ps.setString(10, user.getType());
 
 			ps.executeUpdate();
 
 		} catch (ClassNotFoundException | SQLException e) {
-
 			e.printStackTrace();
 		}
 
@@ -50,8 +40,7 @@ public class UserServiceImpl implements IUserService {
 	public User updateUser(User user) {
 
 		String updateUserQuery = "UPDATE user "
-				+ "SET firstName = ?, lastName = ?, gender = ?, country = ?, platform = ?, email = ? "
-				+ "WHERE userID = ?";
+				+ "SET firstname = ?, lastname = ?, gender = ?, country = ?, platform = ?, email = ? " + "WHERE id = ?";
 
 		try {
 			PreparedStatement ps = DBConnection.getDBconnection().prepareStatement(updateUserQuery);
@@ -67,7 +56,6 @@ public class UserServiceImpl implements IUserService {
 			ps.executeUpdate();
 
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -79,7 +67,7 @@ public class UserServiceImpl implements IUserService {
 		ArrayList<User> arrayList = new ArrayList<>();
 		String uID = null;
 
-		String loginQuery1 = "SELECT * FROM useraccount WHERE userName = ? AND password = ?";
+		String loginQuery1 = "SELECT * FROM user WHERE username = ? AND password = ?";
 
 		PreparedStatement ps;
 		try {
@@ -119,7 +107,6 @@ public class UserServiceImpl implements IUserService {
 			}
 
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -130,7 +117,7 @@ public class UserServiceImpl implements IUserService {
 	public String getPassword(String userID) {
 
 		String password = null;
-		String getPasswordQuery = "SELECT * FROM useraccount WHERE userID = ?";
+		String getPasswordQuery = "SELECT * FROM user WHERE id = ?";
 
 		try {
 			PreparedStatement ps = DBConnection.getDBconnection().prepareStatement(getPasswordQuery);
@@ -141,11 +128,10 @@ public class UserServiceImpl implements IUserService {
 
 			if (resultSet.next()) {
 
-				password = resultSet.getString(3);
+				password = resultSet.getString(8);
 			}
 
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -154,8 +140,8 @@ public class UserServiceImpl implements IUserService {
 
 	public void updatePassword(String userID, String password) {
 
-		String updateUserPasswordQuery = "UPDATE useraccount SET password = ? WHERE userID = ?";
-		String updateUserRecoveryPasswordQuery = "UPDATE passwordrecovery SET password = ? WHERE userID = ?";
+		String updateUserPasswordQuery = "UPDATE user SET password = ? WHERE id = ?";
+		String updateUserRecoveryPasswordQuery = "UPDATE passwordrecovery SET password = ? WHERE id = ?";
 
 		try {
 			PreparedStatement ps = DBConnection.getDBconnection().prepareStatement(updateUserPasswordQuery);
@@ -173,7 +159,6 @@ public class UserServiceImpl implements IUserService {
 			ps.executeUpdate();
 
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -183,7 +168,7 @@ public class UserServiceImpl implements IUserService {
 
 		ArrayList<User> userList = new ArrayList<User>();
 
-		String getUserQuery = "SELECT * FROM user WHERE userID = ?";
+		String getUserQuery = "SELECT * FROM user WHERE id = ?";
 
 		try {
 
@@ -203,13 +188,13 @@ public class UserServiceImpl implements IUserService {
 				user.setCountry(resultSet.getString(5));
 				user.setPlatform(resultSet.getString(6));
 				user.setUserName(resultSet.getString(7));
-				user.setEmail(resultSet.getString(8));
-				user.setType(resultSet.getString(9));
+				user.setPassword(resultSet.getString(8));
+				user.setEmail(resultSet.getString(9));
+				user.setType(resultSet.getString(10));
 				userList.add(user);
 			}
 
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -221,7 +206,7 @@ public class UserServiceImpl implements IUserService {
 
 		ArrayList<String> arrayList = new ArrayList<String>();
 
-		String findIDQuery = "SELECT userID FROM user";
+		String findIDQuery = "SELECT id FROM user";
 
 		PreparedStatement ps;
 		try {
@@ -234,7 +219,6 @@ public class UserServiceImpl implements IUserService {
 			}
 
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -243,17 +227,12 @@ public class UserServiceImpl implements IUserService {
 
 	public void deleteUser(String userID) {
 
-		String deleteUserQuery = "DELETE FROM user WHERE userID = ?";
-		String deleteUserAccountQuery = "DELETE FROM useraccount WHERE userID = ?";
-		String deleteFavouriteQuery = "DELETE FROM favourite WHERE userID = ?";
+		String deleteUserQuery = "DELETE FROM user WHERE id = ?";
+		String deleteFavouriteQuery = "DELETE FROM favourite WHERE id = ?";
 
 		try {
 
 			PreparedStatement ps = DBConnection.getDBconnection().prepareStatement(deleteUserQuery);
-			ps.setString(1, userID);
-			ps.executeUpdate();
-
-			ps = DBConnection.getDBconnection().prepareStatement(deleteUserAccountQuery);
 			ps.setString(1, userID);
 			ps.executeUpdate();
 
@@ -262,7 +241,6 @@ public class UserServiceImpl implements IUserService {
 			ps.executeUpdate();
 
 		} catch (ClassNotFoundException | SQLException e) {
-
 			e.printStackTrace();
 		}
 	}
